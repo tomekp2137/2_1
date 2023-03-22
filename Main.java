da43e744e6d5f00d49de2b6dca7cbf6c8adfbe18
tomekp2137 <127328323+tomekp2137@users.noreply.github.com>
2023-03-22 15:15:58 +0000

    działą

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+    String[] tab = new String[12];
+    tab[0] = "H" ;
+    tab[1] = "e";
+    tab[2] = "l";
+    tab[3] = "l";
+    tab[4] = "o";
+    tab[5] = "";
+    tab[6] = "w";
+    tab[7] = "o";
+    tab[8] = "r";
+    tab[9] = "l";
+    tab[10] = "d";
+    tab[11] = "!";
+
+
+      
+    for (int i=0;i<=11;i++){
+      System.out.println(tab[i]);
+    }
+
+    
   }
 }
